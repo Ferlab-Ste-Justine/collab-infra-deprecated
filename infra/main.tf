6311bd77e778c4ec0e059cc6e51c920a41213aed
Eric Vallee <eric_vallee@webificservices.com>
2020-05-21 02:04:34 -0400

    Added ssh keys setup on bastion

--- infra/main.tf
+++ infra/main.tf
@@ -19,12 +19,22 @@ resource "openstack_networking_floatingip_v2" "bastion_ip" {
   pool = data.openstack_networking_network_v2.external_network.name
 }
 
+data "template_cloudinit_config" "bastion_config" {
+  gzip = true
+  base64_encode = true
+  part {
+    content_type = "text/cloud-config"
+    content = templatefile("${path.module}/bastion_cloud_config.yaml", openstack_compute_keypair_v2.admin_keypair)
+  }
+}
+
 resource "openstack_compute_instance_v2" "bastion" {
   name            = "bastion"
   image_id        = data.openstack_images_image_v2.ubuntu_bionic.id
   flavor_id       = data.openstack_compute_flavor_v2.micro.id
   key_pair        = openstack_compute_keypair_v2.admin_keypair.name
   security_groups = ["default"]
+  user_data = data.template_cloudinit_config.bastion_config.rendered
 
   network {
     name = data.openstack_networking_network_v2.network.name
--- infra/versions.tf
+++ infra/versions.tf
@@ -2,5 +2,6 @@ terraform {
     required_version = "~> 0.12"
     required_providers {
         openstack = "~> 1.28"
+        template = "~> 2.1"
     }
 }
